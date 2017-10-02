e4123a3c00c5f2185f93464ef5e65c2e1a9d5c74
Christie Lynam <christielynam@gmail.com>
2017-10-02 13:16:19 -0600

    add gitignore and normalize files

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+app.get('/', (request, response) => {
+  response.send('Palette Picker!!!');
+});
+
+app.listen(3000, () => {
+  console.log('Express server running on localhost:3000');
+})
