e69622395bb769c88247dbc6c7930fb976280064
Christie Lynam <christielynam@gmail.com>
2017-10-03 12:47:09 -0600

    add projects and palettes table

--- db/migrations/20171002134325_initial.js
+++ db/migrations/20171002134325_initial.js
@@ -1,8 +1,27 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = (knex, Promise) => {
+  return Promise.all([
+    knex.schema.createTable('projects', (table) => {
+      table.increments('id').primary()
+      table.string('name').unique()
+      table.timestamps(true, true)
+    }),
+    knex.schema.createTable('palettes', (table) => {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('hex_val_1')
+      table.string('hex_val_2')
+      table.string('hex_val_3')
+      table.string('hex_val_4')
+      table.string('hex_val_5')
+      table.integer('project_id').unsigned()
+      table.foreign('project_id').references('projects.id')
+    })
+  ])
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = (knex, Promise) => {
+  return Promise.all({
+    knex.schema.dropTable('projects'),
+    knex.schema.dropTable('palettes')
+  })
 };
