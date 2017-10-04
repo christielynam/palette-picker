67776b4d1b4d5680584e0a8efc584eb20fbb5498
Christie Lynam <christielynam@gmail.com>
2017-10-04 12:20:14 -0600

    add initial seed data

--- db/seeds/dev/projects.js
+++ db/seeds/dev/projects.js
@@ -16,12 +16,8 @@ exports.seed = function(knex, Promise) {
         }, 'id')
         .then(project => {
           return knex('palettes').insert([
-            { name: 'Life Savers', hex_val_1: '#678H53', hex_val_2: '#F56J73', hex_val_3: '#24KJ87',
-            hex_val_4: '#8723D5',
-            hex_val_5: '#9E45H6', project_id: project[0] },
-            { name: 'Cotton Candy', hex_val_1: '#678H53', hex_val_2: '#F56J73', hex_val_3: '#24KJ87',
-            hex_val_4: '#8723D5',
-            hex_val_5: '#9E45H6', project_id: project[0] }}
+            { name: 'Life Savers', hex_val_1: '#2B0DBC', hex_val_2: '#7C93FE', hex_val_3: '#DC8AC9', hex_val_4: '#C9309E', hex_val_5: '#B442E9', project_id: project[0] },
+            { name: 'Cotton Candy', hex_val_1: '#135D1D', hex_val_2: '#8AF105', hex_val_3: '#7946B2', hex_val_4: '#27CCB6', hex_val_5: '#874D5E', project_id: project[0] }
           ])
         })
         .then(() => console.log('Seeding complete!'))
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+const bodyParser = require('body-parser');
+
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
-const bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
+
+
 
 app.post('/api/v1/projects', (request, response) => {
   const { name } = request.body;
