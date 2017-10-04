4fc62ece972dd495a9ebf8976ec9b138ffaf155f
Christie Lynam <christielynam@gmail.com>
2017-10-03 22:25:46 -0600

    add palette to db

--- server.js
+++ server.js
@@ -11,10 +11,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
-app.post('/api/v1/projects', (request, response) => {
-  const { name } = request.body;
-  response.status(201).json({ name })
-})
+// app.post('/api/v1/projects', (request, response) => {
+//   const { name } = request.body;
+//
+//   database('projects').insert({ name }, '*')
+//     .then(results => response.status(201).json(results))
+// })
+
+// app.post('/api/v1/palettes', request, response) => {
+//   const { name, hex_val_1, hex_val_2, hex_val_3, hex_val_4, hex_val_5, project_id } = response.body;
+//
+//   database('palettes').insert({ name, hex_val_1, hex_val_2, hex_val_3, hex_val_4, hex_val_5, project_id }, '*')
+//     .then(results => response.status(201).json(results))
+// }
 
 app.get('/api/v1/projects', (request, response) => {
   response.status(200).json(response)
