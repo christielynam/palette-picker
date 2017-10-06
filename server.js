1daf0bba6d162f9710c7c2549a1c71f15026a3e5
Christie Lynam <christielynam@gmail.com>
2017-10-06 17:49:55 -0600

    undo screwup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 
-const environment = process.env.NODE_ENV || 'test';
+const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
