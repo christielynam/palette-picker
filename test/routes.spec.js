0d9a5ec1e2c305dc274acdfb2b58a670ae1517fb
Christie Lynam <christielynam@gmail.com>
2017-10-06 15:11:01 -0600

    add first api test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,6 +3,10 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
@@ -28,5 +32,48 @@ describe('Client Routes', () => {
 });
 
 describe('API Routes', () => {
+
+  before((done) => {
+    database.migrate.latest()
+      .then(() => done())
+      .catch(error => console.log(error));
+  });
+
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => done())
+      .catch(error => console.log(error));
+  });
+
+  describe('GET /api/v1/projects', () => {
+
+    it('should retrieve all projects', (done) => {
+      chai.request(server)
+      .get('/api/v1/projects')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('Project 1');
+        done();
+      });
+    });
+
+    it('should return a 404 if the path is incorrect', (done) => {
+      chai.request(server)
+      .get('/api/v1/poop')
+      .end((error, response) => {
+        response.should.have.status(404);
+
+        done();
+      });
+    });
+
+
+  });
 
 });
