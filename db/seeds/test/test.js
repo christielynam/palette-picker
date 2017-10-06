9c353dd68c3f8797c6a0bffe08bec2aa6aec412b
Christie Lynam <christielynam@gmail.com>
2017-10-06 14:40:08 -0600

    add seed test data

--- db/seeds/test/test.js
+++ db/seeds/test/test.js
@@ -1,13 +1,43 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+
+  return knex('palettes').del() // delete all palettes first
+    .then(() => knex('projects').del()) // delete all projects
+
+    .then(() => {
+      return Promise.all([
+
+        // Insert a single project, return the project ID, insert 2 palettes
+        knex('projects').insert({
+          id: 1,
+          name: 'Project 1'
+        }, 'id')
+        .then(project => {
+          return knex('palettes').insert([
+            {
+              id: 1,
+              name: 'Life Savers',
+              hex_val_1: '#2B0DBC',
+              hex_val_2: '#7C93FE',
+              hex_val_3: '#DC8AC9',
+              hex_val_4: '#C9309E',
+              hex_val_5: '#B442E9',
+              project_id: project[0]
+            },
+            {
+              id: 2,
+              name: 'Cotton Candy',
+              hex_val_1: '#135D1D',
+              hex_val_2: '#8AF105',
+              hex_val_3: '#7946B2',
+              hex_val_4: '#27CCB6',
+              hex_val_5: '#874D5E',
+              project_id: project[0]
+            }
+          ])
+        })
+        .then(() => console.log('Seeding complete!'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ]) 
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
 };
